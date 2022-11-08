b1364ce8750216f5365e3bec68146776a9f40123
Adam Cox <adamcox.dev@gmail.com>
2022-11-08 14:30:27 -0700

    First commit part 2

---
+++ crud.py
@@ -0,0 +1,13 @@
+from model import db, User, Movie, Rating, connect_to_db
+
+
+def create_user(email, password):
+    
+    user = User(email=email, password=password)
+
+    return user
+
+
+if __name__=='__main__':
+    from server import app
+    connect_to_db(app)
