6118433ebf05cb39b99a9425be03c12bf173076a
Adam Cox <adamcox.dev@gmail.com>
2022-11-09 14:34:11 -0700

    Part3 sec 2 step 1,2

--- server.py
+++ server.py
@@ -12,7 +12,14 @@ app = Flask(__name__)
 app.secret_key = 'dev'
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
 
+@app.route('/movies')
+def all_movies():
+    movies = crud.get_movies()
+    return render_template("all_movies.html", movies=movies)
 
 if __name__ == "__main__":
     connect_to_db(app)
